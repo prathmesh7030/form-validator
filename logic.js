29e3f1b7a8109a8eb08a7f709b4e5daac441b5cf
Prathmesh7030 <kolpeprathmesh9960@gmail.com>
2026-09-10 23:04:11 +0530

    Check email is valid

--- logic.js
+++ logic.js
@@ -17,3 +17,13 @@ function showSuccess(input) {
   const formControl = input.parentElement;
   formControl.className = 'form-control success';
 }
+
+// Check email is valid
+function checkEmail(input) {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (re.test(input.value.trim())) {
+    showSuccess(input);
+  } else {
+    showError(input, 'Email is not valid');
+  }
+}
