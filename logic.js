23aade160d66139dca54fe47f2ac6c767a016172
Prathmesh7030 <kolpeprathmesh9960@gmail.com>
2026-09-10 23:06:06 +0530

    Check required fields

--- logic.js
+++ logic.js
@@ -27,3 +27,18 @@ function checkEmail(input) {
     showError(input, 'Email is not valid');
   }
 }
+
+// Check required fields
+function checkRequired(inputArr) {
+  let isRequired = false;
+  inputArr.forEach(function(input) {
+    if (input.value.trim() === '') {
+      showError(input, `${getFieldName(input)} is required`);
+      isRequired = true;
+    } else {
+      showSuccess(input);
+    }
+  });
+
+  return isRequired;
+}
