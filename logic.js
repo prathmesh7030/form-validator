3eb46855dff77abccabc343ca7336ee366182433
Prathmesh7030 <kolpeprathmesh9960@gmail.com>
2026-09-11 20:56:57 +0530

    Check input length

--- logic.js
+++ logic.js
@@ -42,3 +42,20 @@ function checkRequired(inputArr) {
 
   return isRequired;
 }
+
+// Check input length
+function checkLength(input, min, max) {
+  if (input.value.length < min) {
+    showError(
+      input,
+      `${getFieldName(input)} must be at least ${min} characters`
+    );
+  } else if (input.value.length > max) {
+    showError(
+      input,
+      `${getFieldName(input)} must be less than ${max} characters`
+    );
+  } else {
+    showSuccess(input);
+  }
+}
