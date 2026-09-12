6dd1e45e9707f931417f4d27b677c5622c2ab23f
Prathmesh7030 <kolpeprathmesh9960@gmail.com>
2026-09-12 23:05:12 +0530

    Event listeners

--- logic.js
+++ logic.js
@@ -71,3 +71,17 @@ function checkPasswordsMatch(input1, input2) {
 function getFieldName(input) {
   return input.id.charAt(0).toUpperCase() + input.id.slice(1);
 }
+
+
+// Event listeners
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+
+  if(checkRequired([username, email, password, password2])){
+    checkLength(username, 3, 15);
+    checkLength(password, 6, 25);
+    checkEmail(email);
+    checkPasswordsMatch(password, password2);
+  }
+
+});
